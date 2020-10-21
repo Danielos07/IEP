56208e28ce174eda478aceb56750d0fb8bf32727
Dani Moica <dani_moica@yahoo.com>
2020-10-21 22:41:52 +0200

    adaugat lab3

---
+++ LAB3/MDI/MDI.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "MDI.hpp"
+
+int mult(int a, int b) {
+    return a*b;
+    
+}
---
+++ LAB3/src/Root.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "MDI.hpp"
+#include <stdio.h>
+
+void doId() {
+    int a = mult(10,4);
+    printf("%d", a);
+}
+
+int main() {
+
+    doId();
+    
+    return 0;
+}
