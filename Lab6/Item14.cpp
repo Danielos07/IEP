10f87ef553456c3b2cfd8016598da56bb1628806
Danielos07 <dani_moica@yahoo.com>
2020-11-12 22:07:03 +0200

    Add files via upload
    Laborator6

---
+++ Lab6/Item14.cpp
@@ -0,0 +1,11 @@
+class Lock
+{
+public:
+	explicit Lock(Mutex* pm)
+		:mutexPtr(pm, unlock)
+	{
+		lock(mutexPtr.get());
+	}
+private:
+	std::tr1::shared_ptr<Mutex> mutexPtr;
+};
---
+++ Lab6/Item15.cpp
@@ -0,0 +1,17 @@
+class Font
+{
+public:
+    explicit Font(FontHandle fh):
+    f(fh)
+    {}
+
+    ~Font(){releaseFont(f);}
+
+    // explicitly convert Font to FontHandle
+    operator FontHandle() const
+    {return f;}
+ 
+private:
+    FontHandle f;
+};
+
